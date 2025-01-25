e57aba9deedd180a0ec0de5b6ace44e90fc1e0dd
rebat <rbdikmen@gmail.com>
2025-01-25 16:16:58 +0300

    Enable CORS configuration in SecurityConfig.
    Added a new CorsConfigurationSource bean to define CORS policies, allowing origins, methods, and headers. Integrated CORS settings into the SecurityFilterChain to ensure proper cross-origin request handling.

--- src/main/java/com/turkcell/mini_e_commere_hw2/util/security/SecurityConfig.java
+++ src/main/java/com/turkcell/mini_e_commere_hw2/util/security/SecurityConfig.java
@@ -7,40 +7,61 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SecurityConfig {
 
-  private final JwtFilter jwtFilter;
+    private final JwtFilter jwtFilter;
 
-  public SecurityConfig(JwtFilter jwtFilter) {
-    this.jwtFilter = jwtFilter;
-  }
+    public SecurityConfig(JwtFilter jwtFilter) {
+        this.jwtFilter = jwtFilter;
+    }
 
-  @Bean
-  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    http
-            .csrf(AbstractHttpConfigurer::disable)
-            .httpBasic(AbstractHttpConfigurer::disable)
-            .authorizeHttpRequests((req) -> req
-                    .requestMatchers(HttpMethod.GET,"/api/v1/orders/**").authenticated()
-                    .requestMatchers(HttpMethod.POST, "/api/v1/orders").hasAnyAuthority("customer")
-                    .requestMatchers(HttpMethod.GET,"/api/v1/categories/**").permitAll()
-                    .requestMatchers(HttpMethod.GET,"/api/v1/sub-categories/**").permitAll()
-                    .requestMatchers(HttpMethod.GET,"/api/v1/products/**").permitAll()
-                    .requestMatchers(HttpMethod.POST, "/api/v1/products").hasAnyAuthority("seller")
-                    .requestMatchers(HttpMethod.PUT, "/api/v1/products").hasAnyAuthority("seller")
-                    .requestMatchers(HttpMethod.DELETE, "/api/v1/products").hasAnyAuthority("seller")
-                    .requestMatchers("/api/v1/auth/**").permitAll()
-                    .requestMatchers("/api/v1/carts/**").hasAnyAuthority("customer")
-                    .requestMatchers("/api/v1/**").hasAnyAuthority("admin")
-                    .requestMatchers("/v3/api-docs/**",
-                                   "/swagger-ui/**",
-                                   "/swagger-ui.html",
-                                   "/webjars/**").permitAll()
-                    .anyRequest().authenticated()
-            )
-            .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
-    return http.build();
-  }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((req) -> req
+                        .requestMatchers(HttpMethod.GET, "/api/v1/orders/**").authenticated()
+                        .requestMatchers(HttpMethod.POST, "/api/v1/orders").hasAnyAuthority("customer")
+                        .requestMatchers(HttpMethod.GET, "/api/v1/categories/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/v1/sub-categories/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/v1/products/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/v1/products").hasAnyAuthority("seller")
+                        .requestMatchers(HttpMethod.PUT, "/api/v1/products").hasAnyAuthority("seller")
+                        .requestMatchers(HttpMethod.DELETE, "/api/v1/products").hasAnyAuthority("seller")
+                        .requestMatchers("/api/v1/auth/**").permitAll()
+                        .requestMatchers("/api/v1/carts/**").hasAnyAuthority("customer")
+                        .requestMatchers("/api/v1/**").hasAnyAuthority("admin")
+                        .requestMatchers("/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html",
+                                "/webjars/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("Authorization", "Content-Type", "X-Requested-With"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
 }
